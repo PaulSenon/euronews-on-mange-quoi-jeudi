30c7c110b5fcce34f6912622de7c4afa3c7354f3
PaulSenon <paul.senon@gmail.com>
2021-09-24 13:24:03 +0200

    fix error

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,9 @@ import Loader from 'react-loader-spinner';
 
 function getWeekId(date) {
   // Copy date so don't modify original
-  d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+  const d = new Date(
+    Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
+  );
   // Set to nearest Thursday: current date + 4 - current day number
   // Make Sunday's day number 7
   d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
